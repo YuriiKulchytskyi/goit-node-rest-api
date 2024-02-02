7b8ad57b567adcfa78ce45b31d5552f764a177ab
YuriiKulchytskyi <yuriikulchytskyi1@gmail.com>
2024-02-02 20:12:36 +0200

    fixed

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,49 +1,73 @@
-const contactsService = require("../services/contactsServices.js");
-const HttpError = require("../helpers/HttpError.js")
-const controllerWrapper = require('../helpers/controllerWrapper')
+const contactsService = require("../services/contactsServices");
+const HttpError = require("../helpers/HttpError");
+const Joi = require("joi");
 
-const getAllContacts = async(req, res) => {
+const getAllContacts = async (req, res, next) => {
+  try {
     const result = await contactsService.listContacts();
-    res.json(result);
+    res.status(200).json(result);
+  } catch (error) {
+    next(error);
+  }
 };
 
-const getOneContact = async (req, res) => {
+const getOneContact = async (req, res, next) => {
+  try {
     const { id } = req.params;
     const result = await contactsService.getContactById(id);
     if (!result) {
-        throw HttpError(404)
+      throw HttpError(404);
     }
-    res.json(result)
+    res.status(200).json(result);
+  } catch (error) {
+    next(error);
+  }
 };
 
-const deleteContact = async (req, res) => {
-    const { id } = req.params
-    const result = await contactsService.removeContact(id)
+const deleteContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await contactsService.removeContact(id);
     if (!result) {
-        throw HttpError(404)
+      throw HttpError(404);
     }
-    res.json(result)
+    res.status(200).json(result);
+  } catch (error) {
+    next(error);
+  }
 };
 
-const createContact = async (req, res) => {
-    const result = await contactsService.addContact(name, email, phone)
-    res.status(201).json(result)
+const createContact = async (req, res, next) => {
+  try {
+    const result = await contactsService.addContact(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
 };
 
-const updateContact = async (req, res) => {
-    const { id } = req.params
-    const { name, email,phone } = req.body
-    const result = await contactsService.updateContact({id, name, email,phone })
+const updateContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    const result = await contactsService.updateById(id, req.body);
+
+    if (result === "empty") {
+      return res.status(400).json({ message: "Body must have at least one field" });
+    }
     if (!result) {
-        throw HttpError(404, "Not found Book")
+      throw HttpError(404);
     }
-    res.json(result)
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = {
-    getAllContacts: controllerWrapper(getAllContacts),
-    getOneContact: controllerWrapper(getOneContact),
-    deleteContact: controllerWrapper(deleteContact),
-    createContact: controllerWrapper(createContact),
-    updateContact: controllerWrapper(updateContact)
+  getAllContacts,
+  getOneContact,
+  deleteContact,
+  createContact,
+  updateContact,
 };
--- services/contactsServices.js
+++ services/contactsServices.js
@@ -1,57 +1,69 @@
 const fs = require("fs/promises");
 const path = require("path");
-const nanoid = require("nanoid")
+const { nanoid } = require("nanoid");
 
 const contactsPath = path.join(__dirname, "../db/contacts.json");
 
 async function listContacts() {
-    const data = await fs.readFile(contactsPath, "utf-8");
-    return JSON.parse(data);
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
 }
 
 async function getContactById(contactId) {
-    const contacts = await listContacts();
-    const contact = contacts.find(contact => contact.id === contactId);
-    return contact || null;
+  const contacts = await listContacts();
+  const contact = contacts.find((item) => item.id === contactId);
+  return contact || null;
 }
 
 async function removeContact(contactId) {
-    const contacts = await listContacts();
-    const index = contacts.findIndex(contact => contact.id === contactId);
-    if (index === -1) {
-        return null;
-    }
-    const [result] = contacts.splice(index, 1);
-    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-    return result;
+  const contacts = await listContacts();
+  const index = contacts.findIndex((item) => item.id === contactId);
+  if (index === -1) return null;
+  const [result] = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return result;
 }
 
 async function addContact({ name, email, phone }) {
-    const contacts = await listContacts();
-    const newContact = {
-        id: nanoid(),
-        name,
-        email,
-        phone
-    };
-    contacts.push(newContact);
-    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-    return newContact;
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+  };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
 }
 
-async function updateContact({contactId, name, email,phone }) {
-    const contacts = await listContacts()
-    const index = contacts.findIndex(contact => contact.id === contactId)
-    if (index === -1) return null
-    contacts[index] = {id, name, email, phone}
-    await fs.writeFile(contactsPath, JSON.stringify(books, null, 2))
-    return contacts[index]
-}
+const updateById = async (id, data) => {
+  function isEmpty(data) {
+    for (let key in data) {
+      return false;
+    }
+    return true;
+  }
+  const empty = isEmpty(data);
+
+  if (empty) {
+    return "empty";
+  }
+  const contacts = await listContacts();
+  const index = contacts.findIndex((item) => item.id === id);
+  if (index === -1) {
+    return null;
+  }
+  contact = contacts[index];
+  contacts[index] = { id, ...contact, ...data };
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return contacts[index];
+};
 
 module.exports = {
-    listContacts,
-    getContactById,
-    removeContact,
-    addContact,
-    updateContact
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateById,
 };
